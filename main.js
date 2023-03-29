70581ad030ba7e4a8f8c6e638dccb5e64553492b
Eshratjahan17 <eshratjahan701@gmail.com>
2023-03-30 04:36:19 +0600

    issu unsolved

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var hamBurger=document.querySelector(".hamburger");
-var  navMenu=document.querySelector(".navMenu");
+var  navBar=document.querySelector(".navbar");
 // hamburger.onClick=function(){
   
 //     navBar.classList.toggle("active");
 // }
 hamBurger.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
+    navBar.classList.toggle("active");
     console.log("navbar clicked");
   });
