e6bf9f85f59eb63bd38c38681f36d6a209170eaf
Eshratjahan17 <eshratjahan701@gmail.com>
2023-04-01 01:02:40 +0600

    mobile nav issue fixed

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-var hamBurger = document.querySelector(".hamburger");
-var mainBody = document.querySelector(".main");
+// var hamBurger = document.querySelector(".hamburger");
+// var mainBody = document.querySelector(".main");
 
-hamBurger.addEventListener("click", () => {
-  var navBar = document.querySelector(".navbar");
-  navBar.classList.toggle("active");
-  mainBody.classList.toggle("active");
-  console.log("navbar clicked");
-});
-// hamBurger.onClick = function () {
+// hamBurger.addEventListener("click", () => {
 //   var navBar = document.querySelector(".navbar");
 //   navBar.classList.toggle("active");
-// };
+//   mainBody.classList.toggle("active");
+//   console.log("navbar clicked");
+// });
+
+// mobile responsive
+const open=document.querySelector(".mob-open");
+const close=document.querySelector(".mob-close");
+const wrapper = document.querySelector(".mob-wrapper").classList;
+open.addEventListener("click",()=>{
+  wrapper.toggle("opened");
+  wrapper.toggle("close");
+
+});
+close.addEventListener("click",()=>{
+  wrapper.toggle("opened");
+  wrapper.toggle("close");
+
+});
+
+
+
 //Form Validation
 const contactForm = document.getElementById("form");
 const nameInput = document.getElementById("name");
