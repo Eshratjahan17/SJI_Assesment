de4884285517ee76a9ad75b2db494732cee89fff
Eshrat <hasan.techtomorrow@gmail.com>
2023-03-31 14:47:53 +0600

    responsive navbar

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-var hamBurger=document.querySelector(".hamburger");
-var  navBar=document.querySelector(".navbar");
-// hamburger.onClick=function(){
-  
-//     navBar.classList.toggle("active");
-// }
+var hamBurger = document.querySelector(".hamburger");
+
 hamBurger.addEventListener("click", () => {
-    navBar.classList.toggle("active");
-    console.log("navbar clicked");
-  });
+  var navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+  console.log("navbar clicked");
+});
+// hamBurger.onClick = function () {
+//   var navBar = document.querySelector(".navbar");
+//   navBar.classList.toggle("active");
+// };
+//Form Validation
+const contactForm = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+const phoneInput = document.getElementById("phone");
+
+contactForm.addEventListener("submit", function (event) {
+  if (!isNaN(nameInput.value)) {
+    window.alert("Name field Should not contain any number");
+    event.preventDefault();
+  }
+  console.log("FormSubmitted");
+});
